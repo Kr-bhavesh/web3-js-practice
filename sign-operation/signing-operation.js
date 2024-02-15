6eeda63440b584aa2df4795ddb767f4af9479fa7
bhavesh-14 <bhavesh.b@knackroot.com>
2024-02-15 18:47:14 +0530

    updated structure and contract

--- sign-operation/signing-operation.js
+++ sign-operation/signing-operation.js
@@ -1,6 +1,8 @@
+require('dotenv').config({path:'./'})
+console.log();
 const {create} = require('web3-eth-accounts');
 const {Web3} = require('web3')
-const web3 = new Web3('http://127.0.0.1:7545')
+const web3 = new Web3(process.env.GANACHE_URL)
 const my_account = web3.eth.accounts.privateKeyToAccount('0xb0531725d9a26e8cc7e4442299796d23f3532c59db02f9dfb66875b05a12e526')
 const account = create()
 const account_2 = web3.eth.accounts.wallet.create(1);
@@ -10,8 +12,8 @@ async function message_sign()
         const sign_message = await web3.eth.sign(message,account_2[0].address)
         console.log(sign_message);
 }
-message_sign()//signs message using wallet
-console.log(account.sign("Hey there"));//signs data with account
+// message_sign()//signs message using wallet
+// console.log(account.sign("Hey there"));//signs data with account
 async function sign_transaction(){
     const sign_transact = await my_account.signTransaction(
         {
@@ -24,4 +26,4 @@ async function sign_transaction(){
     )
     console.log(sign_transact);
 }
-sign_transaction() //signs a transaction
+// sign_transaction() //signs a transaction
